e2213b216659a0206bfc7aee1f415c1b1c1174cc
Dylan Wetterling <dwetterling@my.wctc.edu>
2022-04-27 01:17:41 -0500

    Added ability to view all details for a specific product

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ namespace NorthwindConsole
                     Console.WriteLine("5) Add a Product");
                     Console.WriteLine("6) Edit a product");
                     Console.WriteLine("7) View Products");
+                    Console.WriteLine("8) View a product details");
                     Console.WriteLine("\"q\" to quit");
                     choice = Console.ReadLine();
                     Console.Clear();
@@ -72,7 +73,7 @@ namespace NorthwindConsole
                             else
                             {
                                 logger.Info("Validation passed");
-                                // TODO: save category to db
+                                
                                 db.AddCategory(category);
                                 logger.Info("Category added - {name}", category.CategoryName);
                             }
@@ -123,18 +124,18 @@ namespace NorthwindConsole
                         var db = new NWConsole_48_DJWContext();
                         Product product = InputProduct(db);
                         if (product != null){
-                            //blog.BlogId = BlogId;
+                            
                             db.AddProduct(product);
-                            logger.Info("Blog added - {name}", product.ProductName);
+                            logger.Info("Product added - {name}", product.ProductName);
                         }
                         
                     }else if (choice == "6"){
                         Console.WriteLine("Choose the product to edit:");
                         var db = new NWConsole_48_DJWContext();
-                        var product = GetProduct(db);
+                        var product = GetProducts(db);
                         if (product != null)
                         {
-                           // input blog
+                           
                             Product UpdatedProduct = InputProduct(db);
                             if (UpdatedProduct != null)
                             {
@@ -150,21 +151,43 @@ namespace NorthwindConsole
                         Console.WriteLine("3) Not Discontinued Produts");
                         
                         string view = Console.ReadLine();
+                        logger.Info($"Option {view} selected");
 
                         if (view == "1"){
                             var db = new NWConsole_48_DJWContext();
                         var product = GetProductName(db);
                         }else if (view == "2"){
+                            Console.WriteLine("Viewing all discontinued products");
                             var db = new NWConsole_48_DJWContext();
                         var product = GetDiscontinuedProducts(db);
                         }else if (view == "3"){
+                            Console.WriteLine("Viewing all non discontinued products");
                             var db = new NWConsole_48_DJWContext();
                         var product = GetNonDiscontinuedProducts(db);
                         }
                         
-                    }
-                    Console.WriteLine();
+                    }else if (choice == "8"){
+                         Console.WriteLine("Choose the product to view details of:");
+                        var db = new NWConsole_48_DJWContext();
+                        var product = GetProducts(db);
 
+                        logger.Info($"product {product.ProductId} selected");
+                    if (product != null)
+                            {
+                                
+                     Console.WriteLine($"Product id: {product.ProductId}");
+                     Console.WriteLine($"Product Name: {product.ProductName}");
+                     Console.WriteLine($"Product Supplier: {product.SupplierId}");
+                     Console.WriteLine($"Product Category ID: {product.CategoryId}");
+                     Console.WriteLine($"Quantity per unit: {product.QuantityPerUnit}");
+                     Console.WriteLine($"Unit Price: {product.UnitPrice}");
+                     Console.WriteLine($"Units in stock: {product.UnitsInStock}");
+                     Console.WriteLine($"Units on order: {product.UnitsOnOrder}");
+                     Console.WriteLine($"Product reorder level: {product.ReorderLevel}");
+                     Console.WriteLine($"Product discontinued: {product.Discontinued} ");                       
+                            }
+                    Console.WriteLine();
+                    }
                 } while (choice.ToLower() != "q");
             }
             catch (Exception ex)
@@ -225,7 +248,7 @@ namespace NorthwindConsole
                             }
                         }return product;
         }
-        public static Product GetProduct(NWConsole_48_DJWContext db)
+        public static Product GetProducts(NWConsole_48_DJWContext db)
         {
             // display all blogs
             var products = db.Products.OrderBy(b => b.ProductId);
@@ -247,7 +270,7 @@ namespace NorthwindConsole
 
         public static Product GetProductName(NWConsole_48_DJWContext db)
         {
-            // display all blogs
+            
             var products = db.Products.OrderBy(b => b.ProductId);
             foreach (Product b in products)
             {
@@ -267,7 +290,7 @@ namespace NorthwindConsole
 
         public static Product GetDiscontinuedProducts(NWConsole_48_DJWContext db)
         {
-            // display all blogs
+            
             var products = db.Products.Where(b => b.Discontinued == true);
             foreach (Product b in products)
             {
@@ -287,7 +310,7 @@ namespace NorthwindConsole
 
         public static Product GetNonDiscontinuedProducts(NWConsole_48_DJWContext db)
         {
-            // display all blogs
+            
             var products = db.Products.Where(b => b.Discontinued == false);
             foreach (Product b in products)
             {
@@ -304,5 +327,28 @@ namespace NorthwindConsole
             logger.Error("Invalid Product Id");
             return null;
         }
+
+        public static Product GetProduct(NWConsole_48_DJWContext db)
+        {
+            
+            var products = db.Products.OrderBy(b => b.ProductId);
+            foreach (Product b in products)
+            {
+                Console.WriteLine($"{b.ProductId}: {b.ProductName}, {b.SupplierId}, {b.CategoryId}, {b.QuantityPerUnit}, {b.UnitPrice}, {b.UnitsInStock}, {b.UnitsOnOrder}, {b.ReorderLevel}, {b.Discontinued}");
+            }
+            if (int.TryParse(Console.ReadLine(), out int ProductId))
+            {
+                Product product = db.Products.FirstOrDefault(b => b.ProductId == ProductId);
+                if (product != null)
+                {
+
+                    return product;
+                    
+                }
+            }
+            logger.Error("Invalid Product Id");
+            return null;
+        }
+
     }
 }
